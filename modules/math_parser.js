3de3208d2feb584d70f8941c82d6f6f5db4a00c9
SkyLark9929 <skyslark9929@gmail.com>
2024-05-09 15:33:17 +0300

    Multiply and add
    now parser can multiply and add in a single expression, even if the
    one or more of the operands are not multiplied.

--- modules/math_parser.js
+++ modules/math_parser.js
@@ -1,32 +1,56 @@
-let expression = '1*2*3+1*3'
+let expression = '1*2*3+1*3+1'
 
 function sumParts(parts){
     let initialValue = 0;
     return parts.reduce((accumulator, currentPart) => accumulator + Number(currentPart), initialValue)
 }
 
-function splitOnPlus(expression){
+function splitOnPlus(expression){ // takes a string, cause if its called, its called first
     let partsSum = expression.split('+');
     return partsSum; //this array consists of strings previously divided by +
 };
 
-function multiplyParts(parts){
-    let initialValue = 1;
+function splitStringOnMultiply(expression){ //takes a string
+    let partsMultiply = expression.split('*');
+    return partsMultiply;
+};
+
+function splitArrayOnMultiply(array){
+    let multipliersGroup;
+    let output = [];
+    for(member of array){
+        multipliersGroup = member.split('*')
+        output.push(multipliersGroup);
+    }
+
+    return output;
+};
+
+function multiplyParts(arrayOfArraysAndDigits){
     let result;
-    let multipliedParts = [];
-    for(part of parts){
-        part = part.split('*');
-        result = part.reduce((accumulator, currentPart) => accumulator * currentPart, initialValue);
-        multipliedParts.push(result);
+    let output = [];
+    let initialValue = 1;
+    for(member of arrayOfArraysAndDigits){
+        if(typeof member == 'object'){ // check whether the member is a group, or it should be bypassed
+            result = member.reduce((accumulator, currentElement) => accumulator * Number(currentElement), initialValue);
+            output.push(result);
+        } else {
+            output.push(multipliersGroup);
+        }
     };
-    return multipliedParts
+    
+    return output;
 }
 
-let plusSplitExpr = splitOnPlus(expression);
-console.log(`Slit on plus: ${plusSplitExpr}`);
+function calculate(expression){
+    if(expression.includes('+') && expression.includes('*')){
+        let dividedByPlusArray = splitOnPlus(expression);
+        let dividedByPlusAndAsteriskArray =  splitArrayOnMultiply(dividedByPlusArray);
+        let multipliedArray = multiplyParts(dividedByPlusAndAsteriskArray);
+        let sum = sumParts(multipliedArray)
 
-let multipliedParts = multiplyParts(plusSplitExpr);
-console.log(`Multiplied parts: ${multipliedParts}`);
+        return sum;
+    }
+};
 
-let sum = sumParts(multipliedParts);
-console.log(`Sum: ${sum}`);
+console.log(calculate(expression))
