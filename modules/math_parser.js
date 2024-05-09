b1ef08a14ba955eaa492c9fc887ff206e29e7e2f
SkyLark9929 <skyslark9929@gmail.com>
2024-05-09 14:36:15 +0300

    Add multiplication in conjunction with addition.

--- modules/math_parser.js
+++ modules/math_parser.js
@@ -1,4 +1,4 @@
-let expression = '1+1+22+323+44'
+let expression = '1*2*3+1*3'
 
 function sumParts(parts){
     let initialValue = 0;
@@ -7,7 +7,26 @@ function sumParts(parts){
 
 function splitOnPlus(expression){
     let partsSum = expression.split('+');
-
-    return partsSum;
+    return partsSum; //this array consists of strings previously divided by +
 };
 
+function multiplyParts(parts){
+    let initialValue = 1;
+    let result;
+    let multipliedParts = [];
+    for(part of parts){
+        part = part.split('*');
+        result = part.reduce((accumulator, currentPart) => accumulator * currentPart, initialValue);
+        multipliedParts.push(result);
+    };
+    return multipliedParts
+}
+
+let plusSplitExpr = splitOnPlus(expression);
+console.log(`Slit on plus: ${plusSplitExpr}`);
+
+let multipliedParts = multiplyParts(plusSplitExpr);
+console.log(`Multiplied parts: ${multipliedParts}`);
+
+let sum = sumParts(multipliedParts);
+console.log(`Sum: ${sum}`);
