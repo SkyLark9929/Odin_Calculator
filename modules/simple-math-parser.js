8a59ef2ff8a3581076edce9bab92dbfefcc7cd45
SkyLark9929 <skyslark9929@gmail.com>
2024-05-11 17:04:24 +0300

    Fixed NaN on negative number multiplication

--- modules/simple-math-parser.js
+++ modules/simple-math-parser.js
@@ -1,22 +1,30 @@
 export function parseMath(string){
     let array = [];
+    let checker;
     let result;
-    
-    if(string.includes('+')){
+    // check for the minus on the first position
+
+    if(string.charAt(0) == '-'){
+        checker = string.slice(1);
+    } else {
+        checker = string;
+    };
+
+    if(checker.includes('+')){
         array = string.split('+');
         result = array.reduce((accumulator, currentValue) => Number(accumulator)+Number(currentValue));
-    } else if(string.includes('-')){
+    } else if(checker.includes('-')){
         array = string.split('-');
         result = array.reduce((accumulator, currentValue) => Number(accumulator)-Number(currentValue));
-    } else if(string.includes('*')){
+    } else if(checker.includes('*')){
         array = string.split('*');
         result = array.reduce((accumulator, currentValue) => Number(accumulator)*Number(currentValue));
-    } else if(string.includes('/')){
+    } else if(checker.includes('/')){
         array = string.split('/');
         result = array.reduce((accumulator, currentValue) => Number(accumulator)/Number(currentValue));
     } else {
         result = string;
-    }
+    };
 
     return result;
 };
